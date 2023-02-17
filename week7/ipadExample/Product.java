efd7c43f51ed34a61645050e36b9a6ec18bea0c1
carolyn-brodie <48027983+carolyn-brodie@users.noreply.github.com>
2023-02-17 16:34:34 -0600

    iPad Example updated

--- week7/ipadExample/Product.java
+++ week7/ipadExample/Product.java
@@ -3,32 +3,30 @@ package week7.ipadExample;
 public class Product {
     private String type;
     private int memorySize; // GigaBytes
-    private boolean cellular;
 
-    public Product(String type, int memory, String cell) {
+
+    public Product(String type, int memory) {
         this.type = type;
         this.memorySize = memory;
-        if (cell.equals("Cellular"))
-            cellular = true;
-        else
-            cellular = false;
+
     }
 
     public double computePrice() {
         if (type.equals("iPad Pro 12.9")) {
-            if (cellular) return ((12 * memorySize) + 160);
-            else return (12 * memorySize);
+            return (8 * memorySize) ;
+
         } else if (type.equals("iPad Pro 11")) {
-            if (cellular) return ((11 * memorySize) + 150);
-            else return (11 * memorySize);
+            return (7 * memorySize);
+
         } else if (type.equals("iPad")) {
-            if (cellular) return ((10 * memorySize) + 140);
-            else return (10 * memorySize);
+            return (5 * memorySize);
+
         } else return 0;
     }
 
+
     public String toString() {
-        return type + " has " + memorySize + "GB memory, cellular is " + cellular +   " and costs $" + computePrice();
+        return type + " has " + memorySize + "GB memory,  and costs $" + computePrice();
     }
 
 }
--- week7/ipadExample/ProductTester.java
+++ week7/ipadExample/ProductTester.java
@@ -2,16 +2,14 @@ package week7.ipadExample;
 
 public class ProductTester {
     public static void main(String[] args)  {
-        Product bigPro = new Product("iPad Pro 12.9", 256, "cellular" );
-        Product pro = new Product("iPad Pro 11", 64, "not cellular");
-        Product iPad = new Product("iPad", 32, "cellular");
+        Product bigPro = new Product("iPad Pro 12.9", 256);
+        Product pro = new Product("iPad Pro 11", 64);
+        Product iPad = new Product("iPad", 32);
 
 
 
         System.out.println(bigPro + "\n" + pro + "\n" +
                 iPad + "\n" );
-
-
     }
 
 }
